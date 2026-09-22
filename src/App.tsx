c0e94763e7b0c0daa626bad0c7bc5971bce22f9b
Tanisha Vyas <165962059+tanishavyastv@users.noreply.github.com>
2026-09-22 13:04:04 +0530

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Products from './components/Products'
+import WhyIndexia from './components/WhyIndexia'
+import FeatureBanners from './components/FeatureBanners'
+import HowItWorks from './components/HowItWorks'
+import Footer from './components/Footer'
+import BackToTop from './components/BackToTop'
+import { useScrollReveal } from './hooks/useScrollReveal'
 
 function App() {
+  useScrollReveal()
+
   return (
     <>
       <Navbar />
       <main>
         <Hero />
+        <Products />
+        <WhyIndexia />
+        <FeatureBanners />
+        <HowItWorks />
       </main>
+      <Footer />
+      <BackToTop />
     </>
   )
 }
